d737935252d2a9293c5033412f873c8d0c1d8758
Ersultan-collab <149644341+Ersultan-collab@users.noreply.github.com>
2023-11-03 16:55:00 +0600

    Create pyto.py

---
+++ python.git/pyto.py
@@ -0,0 +1,41 @@
+# fruits = ['banana','orange','mango','lemon']
+# vegetables = ['tomato','potato','cabbage','onion','carrot']
+# animal_products = ['milk','meat','butter','yoghurt']
+# print('fruits:',fruits)
+# print('Number of fruits:',len(fruits))
+# print('vegetables:',vegetables)
+# print('Number of fruits:',len(vegetables))
+
+# fruits = ['banana','orange','mango','lemon']
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[3])
+# last_index = len(fruits) - 1
+# print(fruits[last_index])
+
+# fruits = ['banana','orange','mango','lemon']
+# print(fruits[-4])
+# print(fruits[-1])
+# print(fruits[-2])
+
+# fruits = ['banana','orange','mango','lemon']
+# first_item, second_item, *rest = fruits
+# print(first_item)
+# print(second_item)
+# print(rest)
+
+# first_item, second_item, third, *rest, ht, ft, tenth = num = [1,2,3,4,5,6,7,8,9,10]
+# print(first_item)
+# print(second_item)
+# print(third)
+# print(rest)
+# print(ht)
+# print(ft)
+# print(tenth)
+
+# fruits = ['banana','orange','mango','lemon']
+# print(fruits[1:3])
+# print(fruits[::-2])
+
+# num = [1,2,3,4,5,6,7,8,9,10]
+# print(num[0:10:4])
