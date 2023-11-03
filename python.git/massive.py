ba6a61aa4e874e94573e5053ac301bdc35c5bda7
Ersultan-collab <149644341+Ersultan-collab@users.noreply.github.com>
2023-11-03 16:49:54 +0600

    Create massive.py

---
+++ python.git/massive.py
@@ -0,0 +1,123 @@
+1-2-3--tap
+lis = ['banana','orange','mango','lemon','peach']
+print(len(lis))
+
+4-tap
+lis = ['banana','orange','mango','lemon','peach']
+print(lis[0])
+print(lis[2])
+print(lis[-1])
+
+5-tap
+mixed_data_types = ['Ersultan',18,1.66,'single','Abay 13']
+print(mixed_data_types)
+
+6-tap
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print(it_companies)
+
+8-tap
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print(f"количество компаний в списке: {len(it_companies)}")
+
+9-tap
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print(f"First Company: {it_companies[0]}")
+print(f"Middle Company: {it_companies[3]}" )
+print(f"Last Company: {it_companies[-1]}" )
+
+11-tap
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+add = it_companies.append('Insatagram')
+print(it_companies)
+
+12-tap
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+add = it_companies.insert(3,'Insatagram')
+print(it_companies)
+
+13-tap
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+for i in range(len(it_companies)):
+        it_companies[i] = it_companies[i].upper()
+
+print(it_companies)
+
+14-tap
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+joi = '# '.join(it_companies)
+print(joi)
+
+16-tap
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+it_companies = sorted(it_companies,reverse=True)
+print(it_companies)
+
+17-tap 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.reverse() 
+print(it_companies) 
+ 
+ 
+18-tap 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+first_company = it_companies[3:] 
+print(first_company) 
+ 
+ 
+19-tap  
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+first_company = it_companies[:-3] 
+print(first_company) 
+ 
+ 
+20-tap 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('Apple') 
+print(it_companies) 
+ 
+ 
+21-tap  
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('Facebook') 
+print(it_companies) 
+ 
+ 
+22-tap 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('Apple') 
+print(it_companies) 
+ 
+ 
+23-tap 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('Amazon') 
+print(it_companies) 
+ 
+ 
+24-tap 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('it_companies') 
+print(it_companies) 
+ 
+lst = ['item1', 'item2'] 
+lst.pop()
+ 
+ 
+25-tap  
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+ 
+ 
+ 
+26-tap  
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux'] 
+back_end = ['Node','Express', 'MongoDB'] 
+integers = front_end + back_end 
+print(integers) 
+ 
+ 
+27-tap 
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux'] 
+back_end = ['Node','Express', 'MongoDB'] 
+integers = front_end + ba
